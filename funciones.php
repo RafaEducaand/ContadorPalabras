9095a42b7a02dcee15fa606906160fc4b9583c8b
AntonioJesusBlanco <ablagui1506@g.educaand.es>
2025-04-17 14:12:21 +0200

    Crear funciones analizarTexto

--- funciones.php
+++ funciones.php
@@ -1,5 +1,12 @@
 <?php
-
+function analizarTexto($texto)
+{
+    $texto = limpiarTexto($texto);
+    $palabras = separarPalabras($texto);
+    $frecuencias = contarPalabras($palabras);
+    ordenarPorFrecuencia($frecuencias);
+    return $frecuencias;
+}
 
 
 ?>
